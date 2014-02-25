e6387d8d8c415db694d0fd73cdf123be6f696da3
Eliad Peller <eliad@wizery.com>
2014-02-25 16:16:28 +0200

    backport of_property_read_bool()
    Signed-off-by: Eliad Peller <eliad@wizery.com>

--- backport/backport-include/linux/of.h
+++ backport/backport-include/linux/of.h
@@ -96,4 +96,26 @@ static inline int of_property_count_u32_elems(const struct device_node *np,
 }
 #endif /* LINUX_VERSION_CODE < KERNEL_VERSION(3,15,0) */
 
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(3,3,0))
+/**
+ * of_property_read_bool - Findfrom a property
+ * @np:		device node from which the property value is to be read.
+ * @propname:	name of the property to be searched.
+ *
+ * Search for a property in a device node.
+ * Returns true if the property exist false otherwise.
+ */
+static inline bool of_property_read_bool(const struct device_node *np,
+					 const char *propname)
+{
+#ifdef CONFIG_OF
+	struct property *prop = of_find_property(np, propname, NULL);
+
+	return prop ? true : false;
+#else
+	return false;
+#endif
+}
+#endif
+
 #endif	/* _COMPAT_LINUX_OF_H */
